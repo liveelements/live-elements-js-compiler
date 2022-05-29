78dcf7a02b96ea88dd5d541d94fdc5467377b1c7
Dinu SV <mail@dinusv.com>
2022-05-29 22:55:30 +0300

    Added support for windows binary download. Updated library paths when loading compiler library.

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
+if ( process.platform === 'win32' ){
+    const { qtHome } = require('./config/qtConfig');
+    process.env.PATH += ';' + qtHome + '/bin'
+}
 const api = require("./build/Release/live_elements_js_compiler.node");
 module.exports = api
