5a5b10ee205f787cfbf079ac4887edac51c9311c
Dinu SV <mail@dinusv.com>
2023-01-12 17:03:24 +0200

    Updated backward-cpp version to support M1.
    Removed qtconfig requirement on windows.
    Added library_unix to macos builds.

--- index.js
+++ index.js
@@ -1,6 +1,2 @@
-if ( process.platform === 'win32' ){
-    const { qtHome } = require('./config/qtConfig');
-    process.env.PATH += ';' + qtHome + '/bin'
-}
 const api = require("./build/Release/live_elements_js_compiler.node");
 module.exports = api
